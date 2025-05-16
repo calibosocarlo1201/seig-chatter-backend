c00c8043b38de0b390dfb4692cf7e7c14d26d02f
calibosocarlo1201 <calibosocarlo1201@gmail.com>
2025-05-17 01:19:04 +0800

    Added DTO Decarator Validation

--- src/users/dto/create-user.input.ts
+++ src/users/dto/create-user.input.ts
@@ -1,10 +1,18 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsEmail, IsStrongPassword } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
   @Field()
+  @IsEmail()
   email!: string;
 
   @Field()
+  @IsStrongPassword(
+    {},
+    {
+      message: "Password must be at least 8 characters long and include at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character"
+    }
+  )
   password!: string;
 }
