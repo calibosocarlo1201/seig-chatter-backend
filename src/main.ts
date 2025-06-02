1421211990be5bbdeec9d1a2d91c8f2858d8da1d
calibosocarlo1201 <calibosocarlo1201@gmail.com>
2025-06-02 22:44:21 +0800

    Set up Graphql Auth Guard

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {bufferLogs: true});
